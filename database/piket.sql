7ae86f2d96b11a110fb2c89243da258f38782bb5
winarwahyuw <winarwahyuw99@gmail.com>
2020-03-29 17:22:42 +0700

    update

--- database/piket.sql
+++ database/piket.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 22, 2020 at 06:22 AM
+-- Generation Time: Mar 29, 2020 at 12:20 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.3
 
@@ -25,49 +25,125 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `jabatan`
+--
+
+CREATE TABLE `jabatan` (
+  `kode_jabatan` varchar(6) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nama_jabatan` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `keterangan` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `jabatan`
+--
+
+INSERT INTO `jabatan` (`kode_jabatan`, `nama_jabatan`, `keterangan`) VALUES
+('dp_mtm', 'Departemen Multimedia', ''),
+('dp_sft', 'Departemen Software', ''),
+('pi', 'Pengurus Inti', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(2, '2020_03_22_110105_create_users_table', 1),
+(3, '2020_03_22_130351_create_jabatan_table', 2),
+(4, '2020_03_22_130922_create_piket_table', 3);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `piket`
 --
 
 CREATE TABLE `piket` (
-  `id_piket` int(3) NOT NULL,
-  `tanggal` date NOT NULL,
+  `id_piket` bigint(20) UNSIGNED NOT NULL,
+  `nim` varchar(13) COLLATE utf8mb4_unicode_ci NOT NULL,
   `mulai_piket` time NOT NULL,
   `selesai_piket` time NOT NULL,
-  `aktifitas` varchar(200) NOT NULL,
-  `status` int(2) NOT NULL,
-  `nim` int(13) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `nim` varchar(13) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nama` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `kode_jabatan` varchar(6) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `periode` varchar(9) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`nim`, `nama`, `kode_jabatan`, `periode`) VALUES
+('3.34.18.0.00', 'admin', 'pi', '2019/2020'),
+('3.34.18.0.03', 'nopi', 'pi', '2019/2020'),
+('3.35.18.2.12', 'ilham', 'dp_sft', '2019/2020');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `jabatan`
+--
+ALTER TABLE `jabatan`
+  ADD PRIMARY KEY (`kode_jabatan`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `piket`
 --
 ALTER TABLE `piket`
-  ADD PRIMARY KEY (`id_piket`),
-  ADD KEY `nim` (`nim`);
+  ADD PRIMARY KEY (`id_piket`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`nim`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `piket`
 --
 ALTER TABLE `piket`
-  MODIFY `id_piket` int(3) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `piket`
---
-ALTER TABLE `piket`
-  ADD CONSTRAINT `piket_ibfk_1` FOREIGN KEY (`nim`) REFERENCES `user` (`nim`);
+  MODIFY `id_piket` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
