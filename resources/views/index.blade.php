7d708ca1e8699c5309ceb2b52c7304d9963c69b7
winarwahyuw <winarwahyuw99@gmail.com>
2020-05-28 21:32:08 +0700

    gantinya coba.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,12 @@
+
+{{-- <form class="form-group">
+    @foreach ($jabatan as $x)
+        {{ $x-> keterangan}}
+        {{ $x-> nama_jabatan}}
+    @endforeach
+</form> --}}
+
+
 @extends('master')
 
 @section('content')
@@ -7,7 +16,7 @@
             Tambah piket
         </button>
 
-        <!-- Modal -->
+        <!-- Start Modal -->
         <div class="modal fade" id="modal-tambah-piket" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
             <div class="modal-content">
@@ -19,43 +28,103 @@
                 </div>
                 <div class="modal-body">
                     {{-- Form tambah Piket start --}}
-                    {!! Form::open() !!}
+                    <form action="{{ url('store')}}" method="POST">
+                        @csrf
+                        {{-- jabatan --}}
                         <div class="form-group">
                             <label for="jabatan">Jabatan</label>
-                            <select id="jabatan" class="form-control">
+                            <select id="jabatan" name="jabatan" class="form-control">
                                 @foreach ($jabatan as $jabatan)
-                            <option value="{{$jabatan->kode_jabatan}}">{{ $jabatan-> nama_jabatan}}</option>
+                                <option value="{{$jabatan->kode_jabatan}}">{{ $jabatan-> nama_jabatan}}</option>
                                 @endforeach
                             </select>
                         </div>
 
+                        {{-- NIM --}}
+                        <div class="form-group">
+                            <label for="nim">NIM</label>
+                            <input class="form-control" type="text" id="nim" placeholder="nim">
+                        </div>
+
+                        {{-- nama --}}
                         <div class="form-group">
                             <label for="nama">Nama</label>
                             <select id="nama" class="form-control">
-                                {{-- @foreach ($nama as $x)
+                                @foreach ($nama as $x)
                                     <option>{{ $x-> nama }}
                                     </option>
-                                @endforeach --}}
+                                @endforeach
 
                                 {{-- COBA --}}
                                 <script>
-                                    var pi='<option selected="selected" value="pi"><?option>';
+                                    $(document).ready(function(){
+                                        $('#jabatan').change(function(){
+                                            $('#nama').children('option').hide();
+                                            $('#nama').children("option[value^=]" + $(this).val().split("")[0]+ "]").show();
+                                        })
+                                    })
                                 </script>
-
                             </select>
                         </div>
-                    {!! Form::close() !!}
-                    {{-- Form tambah Piket End --}}
-                </div>
-                <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
 
-                <button type="submit" class="btn btn-primary">Mulai Piket</button>
-                </div>
+                        {{-- mulai piket --}}
+                        <div class="form-group">
+                            <label for="mulai_piket">Mulai Piket</label>
+                        <input class="form-control" type="datetime-local" name="mulai_piket" id="mulai_piket" value="{{ date("Y-m-d h:i:sa")}}">
+                        </div>
+
+                        </div>
+                        <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+
+                        <button type="submit" class="btn btn-primary" value="Simpan">Mulai Piket</button>
+                        </div>
+                    </form>
+                    {{-- Form tambah Piket End --}}
+
             </div>
             </div>
         </div>
+        {{-- End Modal --}}
+
+        <hr>
+        {{-- Start Table List Piket --}}
+        <div id="list_data_piket">
+            <h2>Daftar Piket</h2>
+            @if (!empty($list_data_piket))
+                <table class="table table-striped table-hover table-responsive">
+                    <thead>
+                        <tr>
+                            <th>No.</th>
+                            <th>Nama</th>
+                            <th>Jabatan</th>
+                            <th>Mulai Piket</th>
+                            <th>Selesai Piket</th>
+                            <th>Aktivitas</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($list_data_piket as $list_piket)
+                            <tr>
+                            <td>{{ $list_piket->id_piket}}</td>
+                            <td>{{ $list_piket->nim}}</td>
+                            <td>Jabatan</td>
+                            <td>{{ $list_piket->mulai_piket}}</td>
+                            <td>{{ $list_piket->selesai_piket}}</td>
+                            <td>{{ $list_piket->aktivitas}}</td>
+                            <td>
+                                <button class="btn btn-warning" type="submit" value="Selesai">Selesai</button>
+                            </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @endif
+        </div>
+        {{-- End Table List Piket --}}
     </div>
 
 
 @endsection
+
