b9868b839bd6134cfec7090cd4fec0ebc26c10ea
winarwahyuw <winarwahyuw99@gmail.com>
2020-05-31 15:45:45 +0700

    update fillable dan relationship

--- app/Jabatan.php
+++ app/Jabatan.php
@@ -7,8 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Jabatan extends Model
 {
     protected $table='jabatan';
+    protected $fillable=['kode_jabatan', 'nama_jabatan', 'keterangan'];
+    protected $primaryKey='kode_jabatan';
 
-    public function jabatanUser(){
-        return $this->hasOne('App\User', 'kode_jabatan');
+    public function jabatan(){
+        return $this->hasMany('App\User', 'kode_jabatan');
     }
 }
