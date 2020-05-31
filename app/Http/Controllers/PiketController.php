0ef5d30ff36f8644a270070d8c1fdab9f85c45ca
winarwahyuw <winarwahyuw99@gmail.com>
2020-05-31 15:47:27 +0700

    tambah fungsi edit dan update

--- app/Http/Controllers/PiketController.php
+++ app/Http/Controllers/PiketController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 use Symfony\Component\Console\Helper\Table;
 
 use App\Piket;
+use App\User;
+use App\Jabatan;
+use Carbon\Traits\Timestamp;
+
+use function Ramsey\Uuid\v1;
 
 class PiketController extends Controller
 {
@@ -13,23 +18,48 @@ class PiketController extends Controller
         //menampilkan data di dropdown modal
         $jabatan=DB::table('jabatan')->get();
         $nama   =DB::table('user')->get();
-        $tanggel=DB::table('piket')->get();
+
+        //join Nama dan Jabatan
+        // $join=DB::table('user')
+        //         ->leftjoin('jabatan','user.kode_jabatan','=','jabatan.kode_jabatan')
+        //         ->select('user.nama','jabatan.nama_jabatan')
+        //         ->get();
+
+        // $kode_jabatan=User::get('kode_jabatan');
+        // $nama_jabatan=Jabatan::where('jabatan.kode_jabatan', '=', 'user.kode_jabatan')->get();
 
         //menampilkan data di table list piket
-        $list_data_piket =  Piket::all();
-        return view('index',
-                    ['jabatan'=> $jabatan,
-                        'nama'=> $nama],
-                    compact('list_data_piket'));
+        $list_data_piket    = Piket::all();
 
+        // $list_data_piket->nama_jabatan=$nama_jabatan;
+        return view('index',
+                    compact('list_data_piket', 'jabatan', 'nama'));
     }
 
     public function store(Request $request){
+        date_default_timezone_set('Asia/Jakarta');
+        $mulai_piket=date('Y-m-d H:i:s', time());
+
         $piket = new Piket;
         $piket->nim=$request->nim;
-        $piket->mulai_piket=$request->mulai_piket;
-        // $piket->selesai_piket=$request->gettimeofday();
+        $piket->mulai_piket=$mulai_piket;
+        $piket->aktivitas=$request->aktivitas;
+        $piket->save();
 
+        return redirect('index');
+    }
+
+    public function editData($id){
+        $piket      =Piket::find($id);
+        $piket->nama=$piket->user->nama;
+        return view('edit', compact('piket'));
+    }
+
+    public function updateData($id, Request $request){
+        $piket=Piket::find($id);
+
+        $piket->selesai_piket=gmdate('H:i:s', (time() + (60 * 60 * 7)));
+        $piket->aktivitas=$request->aktivitas;
         $piket->save();
 
         return redirect('index');
